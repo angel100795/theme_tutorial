0267dd3594096ccaae0ea15e277af474347ecd54
Angel Alvarez Castro <angel.alvarez@quadit.mx>
2020-01-02 18:05:47 -0600

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Quadit.mx',
+
+  'data': ['views/layout.xml',
+  		   'views/pages.xml',
+  		   'views/snippets.xml',
+  		   'views/assets.xml',
+  		   'views/options.xml',
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_blog', 'sale'],
+}
